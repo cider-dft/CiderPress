436c25c5b49801be308bcc61a63587860b9c92e7
Kyle Bystrom <kylebystrom@gmail.com>
2025-01-31 12:05:34 -0500

    misc stuff

--- ciderpress/pyscf/vdw.py
+++ ciderpress/pyscf/vdw.py
@@ -6,9 +6,131 @@ from pyscf.dft.numint import NBINS, _dot_ao_ao_sparse, _scale_ao_sparse, xc_deri
 from ciderpress.dft.plans import libcider
 
 
-def _r2vv10a(rho, coords, vvrho, vvweight, vvcoords, nlc_pars):
+def _get_r2vv10_wh(R, M, Ws, W0, N1, N2):
+    Wh = N1 * W0 / (M**(4.0 / 3) * R**(4.0 / 9))
+    Wh /= (1 + N2 * R**(1.0 / 3))**(2.0 / 3)
+    Wh /= 1 + Ws
+    return Wh
+
+def _r2vv10a(rho, coords, vvrho, vvweight, vvcoords, nlc_pars, with_local_part=True):
     C1, C2, C3, C4, C5 = nlc_pars
-    thresh = 1e-8
+    thresh = 1e-15
+
+    # output
+    exc = numpy.zeros(rho[0, :].size)
+    # vxc = numpy.zeros([2, rho[0, :].size])
+
+    # outer grid needs threshing
+    threshind = rho[0, :] >= thresh
+    coords = coords[threshind]
+    R = rho[0, :][threshind]
+    Gx = rho[1, :][threshind]
+    Gy = rho[2, :][threshind]
+    Gz = rho[3, :][threshind]
+    G = Gx**2.0 + Gy**2.0 + Gz**2.0
+    ind = numpy.argmax(G)
+    #print("CHECK", (G[ind]**0.5 / R[ind])**4, numpy.max(R))
+    #print("CHECK", (G[ind]**0.5 / (2 * (3 * numpy.pi**2)**0.3333333 * R[ind]**1.333333)), numpy.max(R))
+
+    # inner grid needs threshing
+    innerthreshind = vvrho[0, :] >= thresh
+    vvcoords = vvcoords[innerthreshind]
+    vvweight = vvweight[innerthreshind]
+    Rp = vvrho[0, :][innerthreshind]
+    RpW = Rp * vvweight
+    Gxp = vvrho[1, :][innerthreshind]
+    Gyp = vvrho[2, :][innerthreshind]
+    Gzp = vvrho[3, :][innerthreshind]
+    Gp = Gxp**2.0 + Gyp**2.0 + Gzp**2.0
+
+    # constants and parameters
+    Pi = numpy.pi
+    Pi43 = 4.0 * Pi / 3.0
+    # Bvv, Cvv = nlc_pars
+    Bvv = 1
+    Bvv * 1.5 * Pi * ((9.0 * Pi) ** (-1.0 / 6.0))
+    # Beta = ((3.0 / (Bvv * Bvv)) ** (0.75)) / 32.0
+
+    # inner grid
+    Wgp = C3 * Gp / (Rp * Rp)
+    Ws2p = Rp + Wgp * Wgp / Pi43
+    Ws23p = Ws2p**(1.0 / 3)
+    Mp = 1 + C1 / Ws23p
+    Wsp = C5 * Wgp / (C2 * Rp**(2.0 / 3))
+    W0p = (Wgp * Wgp + Pi43 * Rp * Mp / (1 + Wsp)) ** 0.5
+    Whp = _get_r2vv10_wh(Rp, Mp, Wsp, W0p, C4, 10*C2)
+    PRpW = Whp**1.5 * Mp * RpW
+    Qp = W0p * Whp
+
+    # outer grid
+    Wg = C3 * G / (R * R)
+    Ws2 = R + Wg * Wg / Pi43
+    Ws23 = Ws2**(1.0 / 3)
+    M = 1 + C1 / Ws23
+    Wp2 = Pi43 * R * M
+    Ws = C5 * Wg / (C2 * R**(2.0 / 3))
+    W0 = (Wg * Wg + Wp2 / (1 + Ws)) ** 0.5
+    Wh = _get_r2vv10_wh(R, M, Ws, W0, C4, 10*C2)
+    P = Wh**1.5 * M
+    Q = W0 * Wh
+
+    vvcoords = numpy.asarray(vvcoords, order="C")
+    coords = numpy.asarray(coords, order="C")
+    F = numpy.empty_like(R)
+    U = numpy.empty_like(R)
+    W = numpy.empty_like(R)
+    libcider.VXC_r2vv10a(
+        F.ctypes.data_as(ctypes.c_void_p),
+        U.ctypes.data_as(ctypes.c_void_p),
+        W.ctypes.data_as(ctypes.c_void_p),
+        vvcoords.ctypes.data_as(ctypes.c_void_p),
+        coords.ctypes.data_as(ctypes.c_void_p),
+        Qp.ctypes.data_as(ctypes.c_void_p),
+        Q.ctypes.data_as(ctypes.c_void_p),
+        PRpW.ctypes.data_as(ctypes.c_void_p),
+        ctypes.c_int(vvcoords.shape[0]),
+        ctypes.c_int(coords.shape[0]),
+    )
+    # exc is multiplied by Rho later
+    exc[threshind] = 0.5 * F * P
+    if with_local_part:
+        exc[threshind] += (3 * Wh**1.5 * 9 * Wp2**2) / (512 * R * W0**1.5)
+    # vxc[0, threshind] = Beta + F + 1.5 * (U * dKdR + W * dW0dR)
+    # vxc[1, threshind] = 1.5 * W * dW0dG
+    return exc  # , vxc
+
+
+def _get_r2vv10c_terms(R, G, params, get_local_part=True):
+    C1, C2, C3, C4, C5 = params
+    Pi = numpy.pi
+    Pi4 = 4.0 * Pi
+    Pi43 = Pi4 / 3.0
+    Wp2 = Pi4 * R
+    R13 = R**(1.0 / 3)
+    M = 1 + C1 / R13
+    Wg = C3 * G / (R * R)
+    x2 = G / R**(8.0 / 3)
+    mix = 1.0 / (1.0 + C4 * x2)
+    print("MIX", numpy.max(mix), numpy.min(mix))
+    Whr = C2 * Wp2**0.5 / (M**(5.0 / 6) * R**(4.0 / 9))
+    Whr /= (1 + C5 * R13)**(2.0 / 3)
+    Whg = 0.1 * (C4 * R13 / (1 + C4 * R13))**(2.0 / 3)
+    P = mix * M * Whr**1.5 + (1 - mix) * Whg**1.5
+    Q = mix * Wp2 / 3 * M * Whr * Whr + (1 - mix) * Wg * Wg * Whg * Whg
+    # P = mix * M * Whr**1.5 + (1 - mix) * Whg**1.5
+    # Q = mix * Wp2 / 3 * M * Whr * Whr + (1 - mix) * Wg * Wg * Whg * Whg
+    # Q = mix * (Wp2 * M)**0.5 * Whr + (1 - mix) * Wg * Whg
+    Q = Q**0.5
+    if get_local_part:
+        E = 3.0 / 32 * P * P * Pi * Pi / Q**1.5 * R
+    else:
+        E = 0
+    return P, Q, E
+
+
+def _r2vv10c(rho, coords, vvrho, vvweight, vvcoords, nlc_pars, with_local_part=True):
+    C1, C2, C3, C4, C5 = nlc_pars
+    thresh = 1e-15
 
     # output
     exc = numpy.zeros(rho[0, :].size)
@@ -34,41 +156,12 @@ def _r2vv10a(rho, coords, vvrho, vvweight, vvcoords, nlc_pars):
     Gzp = vvrho[3, :][innerthreshind]
     Gp = Gxp**2.0 + Gyp**2.0 + Gzp**2.0
 
-    # constants and parameters
-    Pi = numpy.pi
-    Pi43 = 4.0 * Pi / 3.0
-    # Bvv, Cvv = nlc_pars
-    Bvv = 1
-    Bvv * 1.5 * Pi * ((9.0 * Pi) ** (-1.0 / 6.0))
-    # Beta = ((3.0 / (Bvv * Bvv)) ** (0.75)) / 32.0
-
     # inner grid
-    Wgp = C3 * Gp / (Rp * Rp)
-    # Wsp = numpy.sqrt(Rp ** (2.0 / 3) + C2 * Wgp * Wgp)
-    Wsp = Rp ** (1.0 / 3) + C2 * Wgp
-    Mp = 1 + C1 / Wsp
-    W0p = (Wgp * Wgp + Pi43 * Rp * Mp) ** 0.5
-    Whp = Wsp / numpy.sqrt(C4 + C5 * W0p * W0p)
-    # Whp = 0.1 * numpy.ones_like(W0p)
-    # Whp = 1.0 / (Kvv * (Rp ** (1.0 / 6.0)))
-    PRpW = Whp**1.5 * Mp * RpW
-    Qp = W0p * Whp
+    Pp, Qp, Ep = _get_r2vv10c_terms(Rp, Gp, nlc_pars, False)
+    PRpW = Pp * RpW
 
     # outer grid
-    Wg = C3 * G / (R * R)
-    # Ws = numpy.sqrt(R ** (2.0 / 3) + C2 * Wg * Wg)
-    Ws = R ** (1.0 / 3) + C2 * Wg
-    M = 1 + C1 / Ws
-    Wp2 = Pi43 * R * M
-    W0 = (Wg * Wg + Wp2) ** 0.5
-    Wh = Ws / numpy.sqrt(C4 + C5 * W0 * W0)
-    # Wh = 0.1 * numpy.ones_like(W0)
-    # Wh = 1.0 / (Kvv * (R ** (1.0 / 6.0)))
-    P = Wh**1.5 * M
-    Q = W0 * Wh
-    # dW0dR = (0.5 * Pi43 * R - 2.0 * W0tmp) / W0
-    # dW0dG = W0tmp * R / (G * W0)
-    # dKdR = (1.0 / 6.0) * K
+    P, Q, E = _get_r2vv10c_terms(R, G, nlc_pars, True)
 
     vvcoords = numpy.asarray(vvcoords, order="C")
     coords = numpy.asarray(coords, order="C")
@@ -89,14 +182,16 @@ def _r2vv10a(rho, coords, vvrho, vvweight, vvcoords, nlc_pars):
     )
     # exc is multiplied by Rho later
     exc[threshind] = 0.5 * F * P
-    exc[threshind] += (3 * Wh**1.5 * Wp2**2) / (512 * R * W0**1.5)
+    if with_local_part:
+        exc[threshind] += E
     # vxc[0, threshind] = Beta + F + 1.5 * (U * dKdR + W * dW0dR)
     # vxc[1, threshind] = 1.5 * W * dW0dG
     return exc  # , vxc
 
 
 def nr_nlc_vxc(
-    ni, mol, grids, xc_code, dm, relativity=0, hermi=1, max_memory=2000, verbose=None
+    ni, mol, grids, xc_code, dm, relativity=0, hermi=1, max_memory=2000, verbose=None,
+    r2c=None
 ):
     """Calculate NLC functional and potential matrix on given grids
 
@@ -141,9 +236,14 @@ def nr_nlc_vxc(
     rho = numpy.hstack(vvrho)
 
     exc = 0
-    nlc_coefs = [([0.029, 0.01, 0.05, 40, 40], 1.0)]
+    if r2c is None:
+        # nlc_coefs = [([0.029, 0.01, 0.05, 40, 40], 1.0)]  # for PBE
+        nlc_coefs = [([0.029, 0.25, 0.1, 25, 25], 1.0)]  # for rPW86+PBE
+    else:
+        nlc_coefs = [(r2c, 1.0)]
     for nlc_pars, fac in nlc_coefs:
-        e = _r2vv10a(rho, grids.coords, rho, grids.weights, grids.coords, nlc_pars)
+        # e = _r2vv10a(rho, grids.coords, rho, grids.weights, grids.coords, nlc_pars)
+        e = _r2vv10c(rho, grids.coords, rho, grids.weights, grids.coords, nlc_pars)
         exc += e * fac
     den = rho[0] * grids.weights
     nelec = den.sum()
