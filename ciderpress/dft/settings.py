attempt to fix numerical stability issues for Cu

ciderpress/dft/settings.py
@@ -33,6 +33,7 @@ from ciderpress.dft.feat_normalizer import (
 LDA_FACTOR = -3.0 / 4.0 * (3.0 / np.pi) ** (1.0 / 3)
 CFC = (3.0 / 10) * (3 * np.pi**2) ** (2.0 / 3)
 ALPHA_TOL = 1e-10
+ALPHA_ETA = 0
 
 
 def _check_l1_dots(l1_dots, nl1):
@@ -1852,16 +1853,47 @@ def ds2(rho, sigma):
 
 def get_alpha(rho, sigma, tau):
     cond = rho < ALPHA_TOL
+    # if (rho < 0).any():
+    #    print("NEGATIVE DENSITY", np.min(rho))
     rho = np.maximum(ALPHA_TOL, rho)
     tau0 = get_uniform_tau(rho)
-    tauw = get_single_orbital_tau(rho, np.sqrt(sigma))
+    tauw = sigma / (8 * rho)  # get_single_orbital_tau(rho, np.sqrt(sigma))
+    cond = np.logical_and(cond, tauw > tau)
+    # if (tauw > tau).any():
+    #    amin = np.argmin(tau - tauw, axis=-1)
+    #    print("NEGATIVE ALPHA", tau[..., amin] - tauw[..., amin],
+    #          rho[..., amin], sigma[..., amin], tau[..., amin],
+    #          amin, tau.shape)
     # TODO this numerical stability trick is a bit of a hack.
     # Should make spline support small negative alpha
     # instead, for the sake of clean code and better stability.
-    alpha = np.maximum((tau - tauw), 0) / tau0
+    # alpha = np.maximum((tau - tauw), 0) / tau0
+    alpha = (tau - tauw) / (tau0 + ALPHA_ETA * tauw)
+    # if rho.shape[-1] == 27450:
+    #    alpha[:] = 0
     alpha[cond] = 0
     return alpha
     # return np.maximum((tau - tauw), 0) / tau0
+
+
+"""
+def get_alpha(n, sigma, tau):
+    # tau LSDA
+    aux = (3. / 10.) * (3.0 * np.pi * np.pi)**(2. / 3.)
+    tau_lsda = aux * n**(5. / 3.)
+
+    # von Weisaecker
+    ind = (n != 0.).nonzero()
+    gdms = np.maximum(sigma, 1e-40)  # |nabla rho|^2
+    tau_w = np.zeros(np.shape(n))
+    tau_w[ind] = np.maximum(np.divide(gdms[ind], 8.0 * n[ind]), 1e-40)
+
+    # z and alpha
+    tau_ = np.maximum(tau_w, tau)
+    alpha = np.divide(tau_ - tau_w, tau_lsda)
+    assert alpha.all() >= 0.0
+    return alpha
+"""
 
 
 def dalpha(rho, sigma, tau):
@@ -1877,4 +1909,52 @@ def dalpha(rho, sigma, tau):
     dadn[cond] = 0
     dadsigma[cond] = 0
     dadtau[cond] = 0
+    # if rho.shape[-1] == 27450:
+    #    dadn[:] = 0
+    #    dadsigma[:] = 0
+    #    dadtau[:] = 0
     return dadn, dadsigma, dadtau
+
+
+def _dalpha(rho, sigma, tau):
+    cond = rho < ALPHA_TOL
+    rho = np.maximum(ALPHA_TOL, rho)
+    tau0 = get_uniform_tau(rho)
+    tauw = sigma / (8 * rho)
+    cond = np.logical_and(cond, tauw > tau)
+    denom = tau0 + ALPHA_ETA * tauw
+    dwdn, dwds = -sigma / (8 * rho * rho), 1 / (8 * rho)
+    numer = tau - tauw
+    dadd = -numer / denom / denom
+    dadn = 5.0 / 3 * dadd
+    dadsigma = ALPHA_ETA * dwds * dadd
+    dadn *= rho ** (2.0 / 3) + ALPHA_ETA * dwdn
+    dadn -= dwdn / denom
+    dadsigma -= dwds / denom
+    dadtau = 1 / denom
+    # cond = (tau - tauw) / tau0 < -0.1
+    dadn[cond] = 0
+    dadsigma[cond] = 0
+    dadtau[cond] = 0
+    return dadn, dadsigma, dadtau
+
+
+"""
+def dalpha(n, sigma, tau):
+    # tau LSDA
+    aux = (3. / 10.) * (3.0 * np.pi * np.pi)**(2. / 3.)
+    tau_lsda = aux * n**(5. / 3.)
+
+    # von Weisaecker
+    ind = (n != 0.).nonzero()
+    gdms = np.maximum(sigma, 1e-40)  # |nabla rho|^2
+    tau_w = np.zeros(np.shape(n))
+    tau_w[ind] = np.maximum(np.divide(gdms[ind], 8.0 * n[ind]), 1e-40)
+
+    # z and alpha
+    tau_ = np.maximum(tau_w, tau)
+    alpha = np.divide(tau_ - tau_w, tau_lsda)
+    return _dalpha()
+    assert alpha.all() >= 0.0
+    return alpha
+"""

ciderpress/gpaw/atom_utils.py
@@ -108,6 +108,8 @@ PSETUP_LIST4 = (
     [0, 2, 4, 6, 8, 1, 3, 5, 7, 2, 4, 6, 8, 3, 5, 7, 4, 6, 8],
     [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4],
 )
+PSETUP_LIST3 = PSETUP_LIST2
+PSETUP_LIST4 = PSETUP_LIST2
 
 
 def get_psetup_func_counts(Z, big=False):
@@ -477,10 +479,11 @@ class _PAWCiderContribs:
 
     def vec_radial_vars(self, n_sLg, Y_nL, dndr_sLg, rnablaY_nLv, ae):
         nspin = len(n_sLg)
-        ngrid = n_sLg.shape[-1] * weight_n.size
+        nrad = n_sLg.shape[-1]
+        nsph = weight_n.size
         nx = 5 if self.is_mgga else 4
-        rho_sxg = np.empty((nspin, nx, ngrid))
-        rho_sxg[:, 0] = np.dot(Y_nL, n_sLg).transpose(1, 2, 0).reshape(nspin, -1)
+        rho_sxg = np.empty((nspin, nx, nrad, nsph))
+        rho_sxg[:, 0] = np.dot(Y_nL, n_sLg).transpose(1, 2, 0)  # .reshape(nspin, -1)
         b_vsgn = np.dot(rnablaY_nLv.transpose(0, 2, 1), n_sLg).transpose(1, 2, 3, 0)
         b_vsgn[..., 1:, :] /= self.xcc.rgd.r_g[1:, None]
         b_vsgn[..., 0, :] = b_vsgn[..., 1, :]
@@ -489,7 +492,9 @@ class _PAWCiderContribs:
         N = Y_nL.shape[0]
         e_g = self.xcc.rgd.empty(N).reshape(-1)
         e_g[:] = 0
-        rho_sxg[:, 1:4] = b_vsgn.transpose(1, 0, 2, 3).reshape(nspin, 3, -1)
+        rho_sxg[:, 1:4] = b_vsgn.transpose(1, 0, 2, 3)  # .reshape(nspin, 3, -1)
+        rho_sxg[:, :, 0, :] = rho_sxg[:, :, 1, :]
+        rho_sxg.shape = (nspin, nx, nrad * nsph)
         if self.is_mgga:
             rho_sxg[:, 4] = self.get_kinetic_energy(ae)
         vrho_sxg = np.zeros_like(rho_sxg)
@@ -1125,7 +1130,22 @@ class CiderRadialExpansion:
         n_sLg[:, 0] += nc0_sg
         dndr_sLg = np.dot(D_sLq, dndr_qg)
         dndr_sLg[:, 0] += dnc0dr_sg
+        xcc = self.rcalc.xc.xcc
         nspins, Lmax, nq = D_sLq.shape
+        if ae:
+            tauc = xcc.tauc_g / (np.sqrt(4 * np.pi) * nspins)
+        else:
+            tauc = xcc.tauct_g / (np.sqrt(4 * np.pi) * nspins)
+        print(
+            "MINPAW",
+            ae,
+            rgd.integrate(tauc),
+            rgd.integrate(nc0_sg[0]) / np.sqrt(4 * np.pi),
+            tauc.shape,
+            np.min(tauc),
+            np.min(dnc0dr_sg**2 / (8 * nc0_sg + 1e-10)),
+            np.min(tauc - dnc0dr_sg**2 / (8 * nc0_sg + 1e-10) / np.sqrt(4 * np.pi)),
+        )
         if self.rcalc.mode == "feature":
             # We call it wx_srLq because it is multiplied
             # by the grid weights already

ciderpress/gpaw/atom_utils2.py
@@ -135,6 +135,8 @@ PSETUP_LIST4 = (
     [0, 2, 4, 6, 8, 1, 3, 5, 7, 2, 4, 6, 8, 3, 5, 7, 4, 6, 8],
     [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4],
 )
+PSETUP_LIST3 = PSETUP_LIST2
+PSETUP_LIST4 = PSETUP_LIST2
 
 
 def get_psetup_func_counts(Z, big=False):
@@ -1773,8 +1775,8 @@ class CiderCoreTermProjector:
         self._kernel_kba = facs * np.exp(-expnts * k2_g[:, None, None])
         # self._kernel_bak[na:] = 0.0
         p21 = self._get_p21_matrix()
-        p22 = self._get_p22_matrix(reg=1e-6)
-        self._zmat_l = self._combine_matrices(p21, p22, reg=1e-6)
+        p22 = self._get_p22_matrix(reg=1e-4)
+        self._zmat_l = self._combine_matrices(p21, p22, reg=1e-4)
 
     def get_c_and_df(self, y_skLb):
         t0 = time.monotonic()

ciderpress/gpaw/cider_paw.py
@@ -24,7 +24,7 @@ import numpy as np
 from gpaw.xc.gga import GGA
 from gpaw.xc.mgga import MGGA
 
-from ciderpress.gpaw.atom_utils2 import FastAtomPASDWSlice, FastPASDWCiderKernel
+from ciderpress.gpaw.atom_utils import FastAtomPASDWSlice, FastPASDWCiderKernel
 from ciderpress.gpaw.cider_fft import (
     CIDERPW_GRAD_MODE_FORCE,
     CIDERPW_GRAD_MODE_NONE,
@@ -593,6 +593,7 @@ class CiderMGGAPASDW(CiderPASDW_MPRoutines, CiderMGGA):
 
             taut_sg = np.empty_like(n_sg)
 
+            print("FFTSUM", self.tauct_G.sum() * self.wfs.gd.dv, self.tauct_G.shape)
             for taut_G, taut_g in zip(taut_sG, taut_sg):
                 taut_G += 1.0 / self.wfs.nspins * self.tauct_G
                 self.distribute_and_interpolate(taut_G, taut_g)

ciderpress/gpaw/interp_paw.py
@@ -30,7 +30,7 @@ from scipy.interpolate import interp1d
 from ciderpress.gpaw.gpaw_grids import GCRadialGridDescriptor
 
 
-def create_kinetic_diffpaw(xcc, ny, phi_jg, tau_ypg, _interpc, r_g_new):
+def create_kinetic_diffpaw(xcc, ny, phi_jg, tau_ypg, _interpc, r_g_new, n_qg, d_qg):
     nj = len(phi_jg)
     dphidr_jg = np.zeros(np.shape(phi_jg))
     for j in range(nj):
@@ -39,6 +39,14 @@ def create_kinetic_diffpaw(xcc, ny, phi_jg, tau_ypg, _interpc, r_g_new):
 
     phi_jg = _interpc(phi_jg)
     dphidr_jg = _interpc(dphidr_jg)
+
+    q = 0  # q: common index for j1, j2
+    for j1 in range(nj):
+        for j2 in range(j1, nj):
+            n_qg[q] = phi_jg[j1] * phi_jg[j2]
+            d_qg[q] = phi_jg[j1] * dphidr_jg[j2]
+            d_qg[q] += dphidr_jg[j1] * phi_jg[j2]
+            q += 1
 
     # second term
     for y in range(ny):
@@ -64,14 +72,15 @@ def create_kinetic_diffpaw(xcc, ny, phi_jg, tau_ypg, _interpc, r_g_new):
             for j2, l2, L2 in xcc.jlL[i1:]:
                 temp = Ax_L[L1] * Ax_L[L2] + Ay_L[L1] * Ay_L[L2] + Az_L[L1] * Az_L[L2]
                 temp *= phi_jg[j1] * phi_jg[j2]
-                # temp[1:] /= r_g_new[1:]**2
-                # temp[0] = temp[1]
-                temp /= r_g_new**2
-                if r_g_new[0] == 0:
-                    temp[0] = temp[1]
+                temp[1:] /= r_g_new[1:] ** 2
+                temp[0] = temp[1]
+                # temp /= r_g_new**2
+                # if r_g_new[0] == 0:
+                #    temp[0] = temp[1]
                 tau_ypg[y, p, :] += temp
                 p += 1
             i1 += 1
+    tau_ypg[:, :, 0] = tau_ypg[:, :, 1]
     tau_ypg *= 0.5
 
 
@@ -94,6 +103,8 @@ class DiffPAWXCCorrection:
         Lmax,
         tau_npg=None,
         taut_npg=None,
+        grad_npg=None,
+        gradt_npg=None,
         tauc_g=None,
         tauct_g=None,
         ke_order_ng=True,
@@ -114,8 +125,27 @@ class DiffPAWXCCorrection:
         self.Lmax = Lmax
         self.tau_npg = tau_npg
         self.taut_npg = taut_npg
+        self.grad_npg = grad_npg
+        self.gradt_npg = gradt_npg
         self.tauc_g = tauc_g
         self.tauct_g = tauct_g
+        if True:
+            self.tauc_g = np.maximum(
+                self.tauc_g,
+                np.sqrt(4 * np.pi) * self.dc_g**2 / (8 * self.nc_g + 1e-10),
+            )
+            self.tauct_g = np.maximum(
+                self.tauct_g,
+                np.sqrt(4 * np.pi) * self.dct_g**2 / (8 * self.nct_g + 1e-10),
+            )
+        print(
+            "TAU_DIFFS", np.min(self.tauc_g - self.dc_g**2 / (8 * self.nc_g + 1e-10))
+        )
+        print(
+            "TAU_DIFFS",
+            np.min(self.tauct_g - self.dct_g**2 / (8 * self.nct_g + 1e-10)),
+        )
+
         self.Y_nL = Y_nL
         if self.tau_npg is not None:
             NP = self.tau_npg.shape[1]
@@ -135,6 +165,24 @@ class DiffPAWXCCorrection:
                 self.taut_pg = np.ascontiguousarray(
                     self.taut_npg.transpose(1, 2, 0).reshape(NP, -1)
                 )
+        if self.grad_npg is not None:
+            NP = self.tau_npg.shape[1]
+            if ke_order_ng:
+                # radial coordinate is last index
+                self.grad_pg = np.ascontiguousarray(
+                    self.grad_npg.transpose(1, 0, 2).reshape(NP, -1)
+                )
+                self.gradt_pg = np.ascontiguousarray(
+                    self.gradt_npg.transpose(1, 0, 2).reshape(NP, -1)
+                )
+            else:
+                # angular coordinate is last index
+                self.grad_pg = np.ascontiguousarray(
+                    self.grad_npg.transpose(1, 2, 0).reshape(NP, -1)
+                )
+                self.gradt_pg = np.ascontiguousarray(
+                    self.gradt_npg.transpose(1, 2, 0).reshape(NP, -1)
+                )
 
     @classmethod
     def from_setup(cls, setup, build_kinetic=False, ke_order_ng=True):
@@ -148,7 +196,7 @@ class DiffPAWXCCorrection:
         tab = np.array((2, 10, 18, 36, 54, 86, 118))
         (setup.Z > tab).sum()
         rcut = xcc.rgd.r_g[-1]
-        if setup.Z > 36 and hasattr(setup.rgd, "a") and hasattr(setup.rgd, "b"):
+        if setup.Z > 18 and hasattr(setup.rgd, "a") and hasattr(setup.rgd, "b"):
             rgd = AERadialGridDescriptor(setup.rgd.a, setup.rgd.b, setup.rgd.N)
             gcut = rgd.ceil(rcut)
             rgd = rgd.new(gcut)
@@ -178,6 +226,8 @@ class DiffPAWXCCorrection:
 
         d_qg = [xcc.rgd.derivative(n_g) for n_g in xcc.n_qg]
         dt_qg = [xcc.rgd.derivative(n_g) for n_g in xcc.nt_qg]
+        n_qg = [n_g.copy() for n_g in xcc.n_qg]
+        nt_qg = [nt_g.copy() for nt_g in xcc.nt_qg]
 
         if build_kinetic:
             nii = xcc.nii
@@ -185,11 +235,20 @@ class DiffPAWXCCorrection:
             ng = rgd.r_g.shape[0]
             tau_npg = np.zeros((nn, nii, ng))
             taut_npg = np.zeros((nn, nii, ng))
-            create_kinetic_diffpaw(xcc, nn, xcc.phi_jg, tau_npg, _interpc, rgd.r_g)
-            create_kinetic_diffpaw(xcc, nn, xcc.phit_jg, taut_npg, _interpc, rgd.r_g)
+            grad_npg = np.zeros((nn, nii, ng))
+            gradt_npg = np.zeros((nn, nii, ng))
+            create_kinetic_diffpaw(
+                xcc, nn, xcc.phi_jg, tau_npg, _interpc, rgd.r_g, n_qg, d_qg
+            )
+            create_kinetic_diffpaw(
+                xcc, nn, xcc.phit_jg, taut_npg, _interpc, rgd.r_g, nt_qg, dt_qg
+            )
         else:
+            raise NotImplementedError
             tau_npg = None
             taut_npg = None
+            grad_npg = None
+            gradt_npg = None
 
         return cls(
             rgd,
@@ -204,14 +263,20 @@ class DiffPAWXCCorrection:
             if xcc.nc_corehole_g is not None
             else None,
             xcc.B_pqL,
-            np.array([_interpc(n_g) for n_g in xcc.n_qg]),
-            np.array([_interpc(nt_g) for nt_g in xcc.nt_qg]),
-            np.array([_interpc(n_g) for n_g in d_qg]),
-            np.array([_interpc(nt_g) for nt_g in dt_qg]),
+            # np.array([_interpc(n_g) for n_g in xcc.n_qg]),
+            # np.array([_interpc(nt_g) for nt_g in xcc.nt_qg]),
+            # np.array([_interpc(n_g) for n_g in d_qg]),
+            # np.array([_interpc(nt_g) for nt_g in dt_qg]),
+            n_qg,
+            nt_qg,
+            d_qg,
+            dt_qg,
             xcc.e_xc0,
             xcc.Lmax,
             tau_npg,
             taut_npg,
+            grad_npg,
+            gradt_npg,
             _interpc(core_dens["tauc_g"]),
             _interpc(core_dens["tauct_g"]),
             ke_order_ng=ke_order_ng,
@@ -245,6 +310,7 @@ def calculate_cider_paw_correction(
         dc0_sg[:] = sqrt(4 * pi) / nspins * xcc.dc_g
         dct0_sg[:] = sqrt(4 * pi) / nspins * xcc.dct_g
         if xcc.nc_corehole_g is not None and nspins == 2:
+            print("CORE HOLE", xcc.nc_corehole_g, xcc.dc_corehole_g)
             nc0_sg[0] -= 0.5 * sqrt(4 * pi) * xcc.nc_corehole_g
             nc0_sg[1] += 0.5 * sqrt(4 * pi) * xcc.nc_corehole_g
             dc0_sg[0] -= 0.5 * sqrt(4 * pi) * xcc.dc_corehole_g